d4b9cc53532f0dd23d190d75c4bf8a23b5131993
marccaabay <marc.caabay@paramount.com.ph>
2017-11-01 13:26:43 +0800

    fix error

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -18,20 +18,23 @@ class Admin::OrdersController < Admin::ApplicationController
 		status = 'failed'
 		order = Order.new(name: clientName, order_date: orderDate, user_id: 1)
 
-		if order.save
-			orderId = order.id
+		begin
+			if order.save
+				orderId = order.id
 
-			for i in 0..itemName.length - 1
-				data << { order_id: orderId, item: itemName[i].to_s, quantity: itemQty[i] }
+				for i in 0..itemName.length - 1
+					data << { order_id: orderId, item: itemName[i].to_s, quantity: itemQty[i] }
+				end
+
+				orderItem = OrderItem.create!([ data ])
+
+				if orderItem
+					status = 'success'
+				end
 			end
-
-			orderItem = OrderItem.create!([ data ])
-
-			if orderItem
-				status = 'success'
-			end
+		rescue => error
+			puts "--------#{$!.backtrace}"
 		end
-
 		render json: { status: status }
 	end
 
