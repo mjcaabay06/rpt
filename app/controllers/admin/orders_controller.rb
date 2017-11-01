3225df3864603572a0508cf22d3075e77b6d6295
marccaabay <marc.caabay@paramount.com.ph>
2017-11-01 13:45:08 +0800

    fix

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,31 +8,37 @@ class Admin::OrdersController < Admin::ApplicationController
 	end
 
 	def create
-		param = params[:params]
-		clientName = param[:name]
-		orderDate = param[:date]
-		itemName = param[:itemName].to_s.split(',')
-		itemQty = param[:itemQty].to_s.split(',')
-
 		data = []
 		status = 'failed'
-		order = Order.new(name: clientName, order_date: orderDate, user_id: 1)
 
-		if order.save
-			orderId = order.id
+		begin
+			param = params[:params]
+			clientName = param[:name]
+			orderDate = param[:date]
+			itemName = param[:itemName].to_s.split(',')
+			itemQty = param[:itemQty].to_s.split(',')
 
-			for i in 0..itemName.length - 1
-				data << { order_id: orderId, item: itemName[i].to_s, quantity: itemQty[i] }
+			
+			order = Order.new(name: clientName, order_date: orderDate, user_id: 1)
+
+			if order.save
+				orderId = order.id
+
+				for i in 0..itemName.length - 1
+					data << { order_id: orderId, item: itemName[i].to_s, quantity: itemQty[i] }
+				end
+
+				orderItem = OrderItem.create!([ data ])
+
+				if orderItem
+					status = 'success'
+				end
 			end
 
-			orderItem = OrderItem.create!([ data ])
-
-			if orderItem
-				status = 'success'
-			end
+			render json: { status: status }
+		rescue => error
+			raise "#{$!.message}"
 		end
-		
-		render json: { status: status }
 	end
 
 	def successful
